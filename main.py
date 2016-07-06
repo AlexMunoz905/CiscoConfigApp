3c2e09d1ec6f354b78b2dc6a6364f0c5eb51cb38
Alex Munoz <alex@napend.com>
2016-07-06 14:22:28 -0400

    We dont need the .lower() for the if statements

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ ConPass = input("Do youw ant to configuere the console password?: ").lower()
 SshPass = input("Do you want to configure the Telnet / SSH password?: ").lower()
 
 if ConfIP == "yes":
-    outIP = input("What should the outside IP be?: ").lower()
+    outIP = input("What should the outside IP be?: ")
 
 if EnPass == "yes":
-    enablePass = input("What should the enable password be?: ").lower()
+    enablePass = input("What should the enable password be?: ")
 
 if ConPass == "yes":
-    ConsolePass = input("What should the console password be?: ").lower()
+    ConsolePass = input("What should the console password be?: ")
 
 if SshPass == "yes":
-    TelnetPass = input("What should the Telnet / SSH password be?: ").lower()
+    TelnetPass = input("What should the Telnet / SSH password be?: ")
