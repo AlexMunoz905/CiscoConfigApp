987490e498cbad62767bffebd5175c65584cdccf
Alex Munoz <alex@napend.com>
2016-07-06 14:08:35 -0400

    Adds the questions if they want to config. Asks them what the info should be if they chose yes

---
+++ main.py
@@ -0,0 +1,20 @@
+# Cisco config app
+
+# Gets the questions done so we can then know what they need to configure
+# .lower() sets the answers to all lowercase
+ConfIP = input("Do you want to configure the IP?: ").lower()
+EnPass = input("Do you want to configure the enable password?: ").lower()
+ConPass = input("Do youw ant to configuere the console password?: ").lower()
+SshPass = input("Do you want to configure the Telnet / SSH password?: ").lower()
+
+if ConfIP == "yes":
+    outIP = input("What should the outside IP be?: ").lower()
+
+if EnPass == "yes":
+    enablePass = input("What should the enable password be?: ").lower()
+
+if ConPass == "yes":
+    ConsolePass = input("What should the console password be?: ").lower()
+
+if SshPass == "yes":
+    TelnetPass = input("What should the Telnet / SSH password be?: ").lower()
